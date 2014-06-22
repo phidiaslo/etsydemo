69fac0cbfcd50c01ea4b5a5e7bbf45cb178dc239
Phidias Lo <phidiaslo@gmail.com>
2014-06-23 01:44:15 +0800

    Install and setup Devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,12 @@
 class Listing < ActiveRecord::Base
-	If Rails.env.development?
+	if Rails.env.development?
 	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png"
 	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-else
+	else
 	 has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
 	 :storage => :dropbox,
    	 :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
    	 :path => ":style/:id_:filename"
    	 validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
-  end
+	end
 end
